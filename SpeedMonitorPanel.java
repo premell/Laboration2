3f13b8a80f72da992ff647d1bd1aafa4d64b6c78
Rayinfluense <samuel125807@hotmail.com>
2020-12-07 17:43:18 +0100

    Lagt till speed labels

--- SpeedMonitorPanel.java
+++ SpeedMonitorPanel.java
@@ -5,6 +5,25 @@ import java.util.List;
 
 public class SpeedMonitorPanel extends JPanel {
 
+    /*
+    Som svar på frågorna på Canvas:
+
+    Q: Hur bör MVC lösning vara utformad?
+
+    A: Denna panel bör vara en självstående klass som CarView skapar en
+    instans av, precis som DrawPanel. CarView har ett beroende av denna SpeedMonitorPanel,
+    men SpeedMonitorPanel har inget beroende av någon annan klass. På det viset kan
+    SpeedMonitorPanel användas utan ytterligare beroenden och kan koexistera med vilken
+    annan panel som helst.
+
+    Q: Hur bör de olika komponenterna kommunicera med varandra?
+
+    A: Som sagt är CarView beroende av SpeedMonitorPanel, så den kommer sätta storlek och position
+    av denna panel, men SpeedMonitorPanel tar inga parametrar eller information någon annan stans ifrån.
+    Det enda SpeedMonitorPanel gör är att sätta hastighetsmätare enligt updateSpeedLabel
+    som görs ifrån CarView(som får kommando från CarController).
+     */
+
     private int maxSpeedLabels=5;
     List<JLabel> speedIndicators = new ArrayList<>();
     private int modifyingLabelIndex;
